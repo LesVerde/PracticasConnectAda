c0d39a40af06ca1220482c822875cbb1758d9857
Leslie <ariadsalg@gmail.com>
2022-10-20 12:15:50 -0500

    Implementación de C.Formulario, Error y Paciente
    Se agrego clases a inputs, validaciones, creación de State, para funcionalidad de Formulario. Se creo Error para mostrar mensajes de error en validación de formulario. En App se implementaron props y State para el acumulador de obj. Unicamente se creó estructura de Pacientes

--- citas-react/src/App.jsx
+++ citas-react/src/App.jsx
@@ -1,18 +1,27 @@
 import Header from "./Componentes/header.jsx"
 import ListadoPaciente from "./Componentes/ListadoPaciente.jsx";
 import Formulario from "./Componentes/formulario.jsx";
-import Error from "./Componentes/error.jsx";
-import Paciente from "./Componentes/paciente.jsx";
+
+import { useState } from "react";
 
 function App() {
+
+    const [pacientes, setPacientes] = useState([]);
+    const [paciente, setPaciente]=useState({});
 
     return (
         <div className="container mx-auto mt-15">
             <Header />
-            <Formulario/>
-            <ListadoPaciente />
-            <Error/>
-            <Paciente/>
+            <div className="md:flex" >
+                <Formulario 
+                    pacientes={pacientes} 
+                    setPacientes={setPacientes}
+                    paciente={paciente}
+                    setPaciente={setPaciente} />
+                    
+                <ListadoPaciente />
+            </div>
+
         </div>
     )
 }
--- citas-react/src/Componentes/formulario.jsx
+++ citas-react/src/Componentes/formulario.jsx
@@ -1,77 +1,115 @@
-function Formulario() {
+import { useState, useEffect } from "react";
+import Error from "./error";
+
+
+function Formulario({pacientes, setPacientes}) {
+    const [mascota, setMascota] = useState('');
+    const [propietario, setPropietario] = useState('');
+    const [email, setEmail] = useState('');
+    const [alta, setAlta] = useState('');
+    const [sintomas, setSintomas] = useState('');
+    const [error, setError] = useState(false);
+
+    const validacionFormulario = (e) => {
+        e.preventDefault();
+        if ([mascota, propietario, email, alta, sintomas].includes('')) {
+            console.log('Hay al menos un campo vacío')
+            setError(true)
+            return;
+        }
+        
+        setError(false)
+        const objPaciente={mascota, propietario, email, alta, sintomas}
+        setPacientes([...pacientes,objPaciente])
+        setMascota('');
+        setPropietario('');
+        setEmail('');
+        setAlta('');
+        setSintomas('');
+    }
+
+
+
     return (
         <div className="md:w-1/2 mx-5 lg:w-2/5">
             <h2 className="font-black text-3xl text-center">Registro de Pacientes</h2>
             <p className="mt-5 text-center text-lg mb-10">Añade Pacientes <span className="text-indigo-400 font-bold">Administrarlos</span></p>
 
             <form className="bg-slate-50 rounded-lg py-10 px-5 mb-10 shadow-md"
-                onSubmit={ev => {
-                    ev.preventDefault();
-
-                }}
+                onSubmit={validacionFormulario}
             >
+                {error && 
+                    <Error><p>Todos lo campos son obligatorios</p></Error>
+                    
+                }
                 <div>
-                <label htmlFor="mascota" className="block uppercase font-bold text-gray-700" >Nombre mascota</label>
-                <input
-                    id="mascota"
-                    type="text"
-                    placeholder="Nombre mascota"
-                    className="border-2 w-full p-2 mt-2 placeholder-gray-600 rounded-md"
+                    <label htmlFor="mascota" className="block uppercase font-bold text-gray-700" >Nombre mascota</label>
+                    <input
+                        id="mascota"
+                        type="text"
+                        placeholder="Nombre mascota"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-600 rounded-md"
+                        onChange={(e) => setMascota(e.target.value)}
+                        value={mascota}
 
-                />
+                    />
                 </div>
                 <div>
-                <label htmlFor="propietario" className="block uppercase font-bold text-gray-700 mt-1.5" >Nombre propietario</label>
-                <input
-                    id="propietario"
-                    type="text"
-                    placeholder="Nombre propietario"
-                    className="border-2 w-full p-2 mt-2 placeholder-gray-600 rounded-md"
+                    <label htmlFor="propietario" className="block uppercase font-bold text-gray-700 mt-1.5" >Nombre propietario</label>
+                    <input
+                        id="propietario"
+                        type="text"
+                        placeholder="Nombre propietario"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-600 rounded-md"
+                        onChange={(e) => setPropietario(e.target.value)}
+                        value={propietario}
+                    />
+                </div>
+                <div>
 
-                /> 
+                    <label htmlFor="email" className="block uppercase font-bold text-gray-700 mt-1.5">Correo Electronico</label>
+                    <input
+                        id="name"
+                        type="email"
+                        placeholder="Correo electrónico"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-600 rounded-md"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                    />
                 </div>
                 <div>
+                    <label htmlFor="alta" className="block uppercase font-bold text-gray-700 mt-1.5">Alta</label>
+                    <input
+                        id="fecha"
+                        type="date"
+                        className="border-2 w-full p-2 mt-2 text-gray-600 rounded-md"
+                        onChange={(e) => setAlta(e.target.value)}
+                        value={alta}
+                    />
+                </div>
 
-                <label htmlFor="email" className="block uppercase font-bold text-gray-700 mt-1.5">Correo Electronico</label>
-                <input
-                    id="name"
-                    type="email"
-                    placeholder="Correo electrónico"
-                    className="border-2 w-full p-2 mt-2 placeholder-gray-600 rounded-md"
-                /> 
-                </div>
                 <div>
-                <label htmlFor="alta" className="block uppercase font-bold text-gray-700 mt-1.5">Alta</label>
-                <input
-                    id="fecha"
-                    type="date"
-                    className="border-2 w-full p-2 mt-2 text-gray-600 rounded-md"
-                />
+                    <label htmlFor="sintomas" className="block uppercase font-bold text-gray-700 mt-1.5">Sintomas</label>
+                    <textarea
+                        id="sintomas"
+                        name="sintomas"
+                        placeholder="Sintomas"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-600 rounded-md"
+                        onChange={(e) => setSintomas(e.target.value)}
+                        value={sintomas}
+                    />
                 </div>
-
-                {/* <label htmlFor="hora" className="block uppercase font-bold text-gray-700">Hora de Cita</label>
-                <input
-                    id="hora"
-                    type="time"
-                    className="border-2 w-full p-2 mt-2 placeholder-gray-600 rounded-md"
-                /> */}
-                <div>
-                <label htmlFor="sintomas" className="block uppercase font-bold text-gray-700 mt-1.5">Sintomas</label>
-                <textarea
-                    id="sintomas"
-                    name="sintomas"
-                    placeholder="Sintomas"
-                    className="border-2 w-full p-2 mt-2 placeholder-gray-600 rounded-md"
-                />
-                </div>
+                <input type="submit" className="bg-indigo-500 text-white font-bold uppercase hover:bg-indigo-700 cursor-pointer transition-colors p-2 rounded-md;" value={'Agregar'} />
             </form>
 
+        
 
 
         </div >
 
     )
-
+    
+    
 
 }
 
